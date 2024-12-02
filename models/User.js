a0f42e3f220c122a417c5cb8df082cb6135b3d1c
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2024-12-02 15:57:35 +0200

    User.js

--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["teacher", "student"], required: true }, // role is required and has specific values
-    preferredLanguage: { type: String, default: "en" }, // default value is "en" if not provided
+    role: { type: String, enum: ["teacher", "student"], required: true }, // role and its specific values
+    preferredLanguage: { type: String, default: "en" }, // default value is "en" 
   },
   { timestamps: true } // Automatically adds createdAt and updatedAt fields
 );
